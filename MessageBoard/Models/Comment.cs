edfe3668ab04a9ded9d50d9c25bcb7923fd4acb6
ryanh <ryanh@LAPTOP-BMF4POHJ>
2021-06-15 00:07:19 -0500

    Polished the View page

--- MessageBoard/Models/Comment.cs
+++ MessageBoard/Models/Comment.cs
@@ -17,6 +17,7 @@ namespace TicketTracker.Models
 		public string UserName { get; set; }
 
 		[Display(Name = "Creation Time")]
+		[DisplayFormat(DataFormatString = "{0:MMM d, yyyy h:mm tt K}")]
 		public DateTime CreationTime { get; set; }
 
 		public int ParentTicketId { get; set; }
--- MessageBoard/wwwroot/js/site.js
+++ MessageBoard/wwwroot/js/site.js
@@ -2,79 +2,106 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 
+/**************** Project View ****************/
+
+// Reloads the page with a GET request including all the filters, search, etc.
+function submitForm() {
+	document.getElementById("project-view-form").submit();
+}
+
+// Called by View Project table column headers (Category, Type, etc.) Sorts the table by that column. Sets OrderByColumn and OrderDirection.
+function tableHeader(header) {
+	document.getElementById("orderByDirection_input").value =
+		document.getElementById("orderByColumn_input").value == header
+			&& document.getElementById("orderByDirection_input").value == "ASC"
+			? "DESC"
+			: "ASC";
+
+	document.getElementById("orderByColumn_input").value = header;
+
+	submitForm();
+}
+
+
+////////// Filter menu //////////
+const cols = {
+	"Categories": ["Bug", "Feature", "Style", "Change"],
+	"Statuses": ["Unassigned", "Open", "Resolved", "Cancelled"],
+	"Priorities": ["Critical", "High", "Medium", "Low"]
+}
+
+// this is a helper function because of JS's closure trait 
+function allCheckboxCallback(key) {
+	return function () {
+		allCheckbox(key);
+	}
+}
+
+// this is called when any filter-menu SelectAll checkbox is clicked
+const allCheckbox = (key) => {
+	const selectAll = document.getElementById("All" + key);
+	for (var ele in cols[key]) {
+		document.getElementById(cols[key][ele]).checked = selectAll.checked;
+	}
+	checkboxClicked();
+}
+
+// update the SelectAll checkbox to reflect current reality
+const updateFilters = () => {
+	for (var key in cols) {
+		var allSelected = true;
+		for (var ele in cols[key]) {
+			if (!document.getElementById(cols[key][ele]).checked) {
+				allSelected = false;
+				break;
+			}
+		}
+		document.getElementById("All" + key).checked = allSelected ? true : false;
+	}
+}
+
+// called when any non-SelectAll checkbox is clicked
+const checkboxClicked = () => {
+	// step 1: apply the new filters
+	updateFilters();
+
+	// step 1.5: make sure the dropdown menu is open when page reloads
+	document.getElementById("ShowFilterDropdown").value = true;
+
+	// step 2: reload the page with the new filters
+	submitForm();
+}
+
+// attach event listeners to checkboxes
+for (var key in cols) {
+	// event listener for each SelectAll checkbox
+	document.getElementById("All" + key).addEventListener("click", allCheckboxCallback(key));
+	for (var ele in cols[key]) {
+		// event listener for other checkboxes
+		document.getElementById(cols[key][ele]).addEventListener("click", function () { checkboxClicked() });
+	}
+}
+
+// Used for the filter menu popup visibilty
+$(function () {
+	$('[data-toggle="popover"]').popover()
+})
+
+
+/********** On load ***********/
 $(document).ready(function () {
 
+	// Used for the filter menu popup visibilty (from bootstrap docs)
 	$('.popover-dismiss').popover({
 		trigger: 'focus'
 	})
 
+	// Update checkboxes in filter menu
+	updateFilters();
 
-	$(function () {
-		$('[data-toggle="popover"]').popover()
-	})
-
+	// Restore visible/hidden state of view-project page filter menu
 	if (document.getElementById("ShowFilterDropdown").value) {
 		document.getElementById("filter-dropdown-button").click();
 	}
 
-
-	const cols = {
-		"Categories": ["Bug", "Feature", "Style", "Change"],
-		"Statuses": ["Unassigned", "Open", "Resolved", "Cancelled"],
-		"Priorities": ["Critical", "High", "Medium", "Low"]
-	}
-
-	// this is a helper function because of JS's closure trait 
-	function allCheckboxCallback(key) {
-		return function () {
-			allCheckbox(key);
-		}
-	}
-
-	// this is called when any SelectAll checkbox is clicked
-	const allCheckbox = (key) => {
-		const selectAll = document.getElementById("All" + key);
-		for (var ele in cols[key]) {
-			document.getElementById(cols[key][ele]).checked = selectAll.checked;
-		}
-		checkboxClicked();
-	}
-
-	const updateFilters = () => {
-		// update the SelectAll checkbox to reflect current reality
-		for (var key in cols) {
-			var allSelected = true;
-			for (var ele in cols[key]) {
-				if (!document.getElementById(cols[key][ele]).checked) {
-					allSelected = false;
-					break;
-				}
-			}
-			document.getElementById("All" + key).checked = allSelected ? true : false;
-		}
-	}
-
-	// this is called when any non-SelectAll checkbox is clicked
-	const checkboxClicked = () => {
-		// step 1: apply the new filters
-		updateFilters();
-
-		// step 1.5: make sure the dropdown menu is open when page reloads
-		document.getElementById("ShowFilterDropdown").value = true;
-
-		// step 2: reload the page with the new filters
-		document.getElementById("project-view-form").submit();
-	}
-
-	// attach event listeners to checkboxes
-	for (var key in cols) {
-		// event listener for each SelectAll checkbox
-		document.getElementById("All" + key).addEventListener("click", allCheckboxCallback(key));
-		for (var ele in cols[key]) {
-			// event listener for other checkboxes
-			document.getElementById(cols[key][ele]).addEventListener("click", function () { checkboxClicked() });
-		}
-	}
-
-	updateFilters();
 });
